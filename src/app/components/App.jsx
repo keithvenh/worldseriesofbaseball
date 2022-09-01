e13abe08587781dc4e163ee882f6b8af2bf35e60
keithvenh <keith.venhuizen@gmail.com>
2022-08-31 16:25:28 -1000

    Add Navigation

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -7,13 +7,43 @@ class App extends Component {
     this.state = {
       view: <SuperAdvanced />
     }
+
+    this.changeView = this.changeView.bind(this);
+  }
+
+  changeView = (page) => {
+    let view;
+    switch(page) {
+      case 'home':
+        view = <p>WELCOME</p>;
+        break;
+      case 'fielding':
+        view = <SuperAdvanced />;
+        break;
+      case 'scorecard':
+        view = <Scorecard />
+        break;
+      default:
+        view = <p>WELCOME</p>;
+    }
+
+    this.setState({view: view})
   }
 
   render() {
 
     return (
       <div className="App">
-          <h1 className='title'>World Series of Baseball</h1>
+          <header className='header'>
+            <h1 className='title' onClick={() => this.changeView('home')}>World Series of Baseball</h1>
+            <nav className='navBar'>
+              <ul className='links'>
+                <li className='linkItem' onClick={() => this.changeView('home')}>Home</li>
+                <li className='linkItem' onClick={() => this.changeView('fielding')}>Fielding</li>
+                <li className='linkItem' onClick={() => this.changeView('scorecard')}>Scorecard</li>
+              </ul>
+            </nav>
+          </header>
           {this.state.view}
       </div>
     );
