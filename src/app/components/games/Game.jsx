393722dc094125e4041f9df2fb908bf13ca89b55
keithvenh <keith.venhuizen@gmail.com>
2022-12-05 17:14:37 -0600

    Add Design to Single Game Box Score

--- src/app/components/games/Game.jsx
+++ src/app/components/games/Game.jsx
@@ -1,11 +1,15 @@
-import {useState} from 'react';
-import { updateDoc, doc } from 'firebase/firestore';
+import {useState, useEffect } from 'react';
+import { updateDoc, doc, getDoc, collection, getDocs, query } from 'firebase/firestore';
 import { db } from '../../../db/db';
 
 export default function Game(props) {
 
     const [game, setGame] = useState(props.options.game);
     const [mode, setMode] = useState('view');
+    const [teams, setTeams] = useState(null);
+    const [games, setGames] = useState(null);
+    const visFlag = require('../../assets/images/' + game.visitor + '.webp')
+    const homeFlag = require('../../assets/images/' + game.home + '.webp')
 
     function updateGameStats(e) {
         e.preventDefault();
@@ -25,27 +29,114 @@ export default function Game(props) {
         })
     }
 
+    async function fetchGames() {
+
+        let gamesRef = collection(db, "leagues/1/games")
+        const q = query(gamesRef);
+        const querySnapshot = await getDocs(q);
+        let allGames = querySnapshot.docs.map((doc) => {
+            return doc.data();
+        })
+
+        setGames(allGames);
+    } 
+
+    function getRecord(team, game = 1000) {
+        const searchGames = games.filter(g => g.id <= game)
+        let wins = searchGames.filter(g => g.winTeam === team).length
+        let losses = searchGames.filter(g => g.loseTeam === team).length
+        return `${wins}-${losses}`
+    }
+
+    async function fetchTeams() {
+        let visTeam = await getDoc(doc(db, "leagues/1/teams", game.visitor));
+        let homeTeam = await getDoc(doc(db, "leagues/1/teams", game.home));
+
+        setTeams({visitor: visTeam.data(), home: homeTeam.data()});
+    }
+
+    useEffect(() => {
+        fetchTeams();
+        fetchGames();
+    },[])
+
+    if(!games || !teams) {
+        return <div>Loading...</div>
+    }
+
     if(mode === 'view') {
 
         return (
-            <div key={game.id} className='gameSummary'>
-                <div className='headers'>
-                    <p className='gameNumber'>GAME {game.id}</p>
-                    <p className='runs'>R</p>
-                    <p className='hits'>H</p>
-                    <p className='err'>E</p>
-                </div>
-                <div className={`${game.winTeam == game.visitor} visitor`}>
-                    <p className='team visTeam'>{game.visitor.toUpperCase()}</p>
-                    <p className='runs visRuns'>{game.visRuns}</p>
-                    <p className='hits visHits'>{game.visHits}</p>
-                    <p className='err visErrors'>{game.visErrors}</p>
-                </div>
-                <div className={`${game.winTeam == game.home} home`}>
-                    <p className='team homeTeam'>{game.home.toUpperCase()}</p>
-                    <p className='runs homeRuns'>{game.homeRuns}</p>
-                    <p className='hits homeHits'>{game.homeHits}</p>
-                    <p className='err homeErrors'>{game.homeErrors}</p>
+            <div>
+
+                <div key={game.id} className='gameSummaryFull'>
+                    <div className='teamDisplay visTeamDisplay'>
+                        <div className='teamFlag'>
+                            <img src={visFlag} alt={`${teams.visitor.name} Logo`} />
+                        </div>
+                        <div>
+                            <p className='teamName'>{teams.visitor.name}</p>
+                            <p className='teamRecord'>{getRecord(game.visitor, game.id)}</p>
+                        </div>
+                        <p className='teamScore'>{game.visRuns}</p>
+                    </div>
+                    <table className='boxscore'>
+                        <tr className='headers'>
+                            <th className='gameNumber'>GAME {game.id}</th>
+                            <th className='inning inning1'>1</th>
+                            <th className='inning inning2'>2</th>
+                            <th className='inning inning3'>3</th>
+                            <th className='inning inning4'>4</th>
+                            <th className='inning inning5'>5</th>
+                            <th className='inning inning6'>6</th>
+                            <th className='inning inning7'>7</th>
+                            <th className='inning inning8'>8</th>
+                            <th className='inning inning9'>9</th>
+                            <th className='runs'>R</th>
+                            <th className='hits'>H</th>
+                            <th className='err'>E</th>
+                        </tr>
+                        <tr className={`${game.winTeam == game.visitor} visitor`}>
+                            <td className='team visTeam'>{game.visitor.toUpperCase()}</td>
+                            <td className='inning inning1'>-</td>
+                            <td className='inning inning2'>-</td>
+                            <td className='inning inning3'>-</td>
+                            <td className='inning inning4'>-</td>
+                            <td className='inning inning5'>-</td>
+                            <td className='inning inning7'>-</td>
+                            <td className='inning inning6'>-</td>
+                            <td className='inning inning8'>-</td>
+                            <td className='inning inning9'>-</td>
+                            <td className='runs visRuns'>{game.visRuns}</td>
+                            <td className='hits visHits'>{game.visHits}</td>
+                            <td className='err visErrors'>{game.visErrors}</td>
+                        </tr>
+                        <tr className={`${game.winTeam == game.home} home`}>
+                            <td className='team homeTeam'>{game.home.toUpperCase()}</td>
+                            <td className='inning inning1'>-</td>
+                            <td className='inning inning2'>-</td>
+                            <td className='inning inning3'>-</td>
+                            <td className='inning inning4'>-</td>
+                            <td className='inning inning5'>-</td>
+                            <td className='inning inning7'>-</td>
+                            <td className='inning inning6'>-</td>
+                            <td className='inning inning8'>-</td>
+                            <td className='inning inning9'>-</td>
+                            <td className='runs homeRuns'>{game.homeRuns}</td>
+                            <td className='hits homeHits'>{game.homeHits}</td>
+                            <td className='err homeErrors'>{game.homeErrors}</td>
+                        </tr>
+                    </table>
+                    <div className='teamDisplay homeTeamDisplay'>
+                        <p className='teamScore'>{game.homeRuns}</p>
+                        <div>
+                            <p className='teamName'>{teams.home.name}</p>
+                            <p className='teamRecord'>{getRecord(game.home, game.id)}</p>
+                        </div>
+                        <div className='teamFlag'>
+                            <img src={homeFlag} alt={`${teams.home.name} Logo`} />
+                        </div>
+                    </div>
                 </div>
                 <div id='editButton' className='button edit' onClick={() => {setMode('edit')}}><i className='fa-solid fa-pencil'></i> Edit</div>
             </div>
